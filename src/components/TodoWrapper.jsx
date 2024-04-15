320627ede4fe904733de39b2a9f2e4239b6d1e97
rahulporel51 <rahulporel51@gmail.com>
2024-04-15 23:07:18 +0530

    add localstorage to app

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import { Todo } from "./Todo";
 import Todo from "./Todo";
 import { TodoForm } from "./TodoForm";
@@ -10,37 +10,52 @@ import { EditTodo } from "./EditTodo";
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
 
-  const addTodo = (todo) => {
-    setTodos([
-      ...todos,
-      { id: uuidv4(), task: todo, completed: false, isEditing: false },
-    ]);
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    setTodos(savedTodos);
+  }, []);
+
+  const updateLocalStorage = (newTodos) => {
+    localStorage.setItem("todos", JSON.stringify(newTodos));
   };
 
-  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+  const addTodo = (todo) => {
+    const newTodos = [
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ];
+    setTodos(newTodos);
+    updateLocalStorage(newTodos);
+  };
 
   const toggleComplete = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
+    const newTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
     );
+    setTodos(newTodos);
+    updateLocalStorage(newTodos);
+  };
+
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+    updateLocalStorage(newTodos);
   };
 
   const editTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
-      )
+    const newTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
     );
+    setTodos(newTodos);
+    updateLocalStorage(newTodos);
   };
 
   const editTask = (task, id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
-      )
+    const newTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
     );
+    setTodos(newTodos);
+    updateLocalStorage(newTodos);
   };
 
   return (
